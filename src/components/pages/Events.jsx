092d02c238e2240fea1a43b1a9e00889c7bc79fa
JuanELopezC <juan.lopez@my.maryvillecollege.edu>
2026-04-08 09:26:04 -0400

    updated events

--- src/components/pages/Events.jsx
+++ src/components/pages/Events.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import Calendar from "../Calendar.jsx";
 import ImageCard from "../ImageCard.jsx";
 import "./Events.css";
+import "../PrintCard.jsx";
+import PrintCard from "../PrintCard.jsx";
+import Slideshow from "../slideshow.jsx";
 
 const Events = () => {
   const events = [
@@ -24,6 +27,33 @@ const Events = () => {
     },
   ];
 
+const eventsRecap = [
+  { 
+    title: "3D Print Workshop", 
+    author: "MC3D", 
+    description: "a workshop held to introduce the basics of 3d printing and get students started on a new journey", 
+    images: ["bradley.jpg", "firstworkshop.jpg", "kelvin.jpg"], 
+    facts: ["10 participants", "2 hours"] 
+  },
+  {
+    title: "Fidget Toys",
+    author: "MC3D",
+    description: "In partnership with the career center we have found and designed some 3D printer fidget toys found around campus",
+    images: ["dinos.jpg", "keys.jpg"],
+    facts: ["Flexy dinos", "infinite cubes", "flexy dragons"]
+  },
+  {
+    title:"Casino Night",
+    author: "MC3D, Scotslife, Math Club, CSC",
+    description: "Las Vegas night style at Issac's!. over 100+ students joined us to play different Vegas style games winning many prices\
+    up to a Nintendo Switch.",
+    images: ["Blackjack!.jpg", "blackjack.jpg", "chancemic.jpg","crapstable.jpg","firstfloorpic.jpg","frontable.jpeg"],
+    facts:["over 100+ students", "over 10+ faculty and staff", "5+ Vegas style games"]
+  }
+
+];
+
+
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [todayEvents, setTodayEvents] = useState([]);
 
@@ -43,6 +73,20 @@ const Events = () => {
   return (
     <div className="events-page">
       <h1 className="events-title">Club Events</h1>
+
+
+            {/*Upcoming Events*/}
+      <section className="slideshow-section">
+        <Slideshow 
+        images= {[
+          '/images/reveal.png',
+          '/images/Roulette.png',
+          
+        ]}
+        />
+
+        
+      </section>  
 
       <div className="events-layout">
         {/* Calendar Section + Events Today */}
@@ -70,14 +114,14 @@ const Events = () => {
 
         {/* Past Highlights Section */}
         <div className="imagecards-section">
-          <h2>Past Highlights</h2>
+          <h2>Some of Our Events</h2>
           <div className="imagecards-grid">
-            <ImageCard picture="braxtonfall.jpg" text="Face of the Club" height="220px" />
-            <ImageCard picture="rocks.jpg" text="Covenant Stone" height="220px" />
-            <ImageCard picture="trophy.jpg" text="3D Trophy and Helment" height="220px" />
+            {eventsRecap.map((p,i) => <PrintCard key= {i} {...p}/>)}
           </div>
         </div>
       </div>
+
+
     </div>
   );
 };
